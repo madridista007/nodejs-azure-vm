e3e63f82c1f8ee230b0e3854b9552431a1f842d0
madridista007 <realmadrid07@live.co.uk>
2025-05-31 15:15:59 +0100

    Test CI/CD auto-deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js on Azure VM! Test pipline");
+  res.send("Hello from Node.js on Azure VM! Test pipliness");
 });
 
 app.listen(PORT, () => {
